99cc02df05ab5effdb88adfabfe2be0e392e18cb
Shinjinwoo <jinu712@gmail.com>
2020-10-20 12:05:51 +0900

    2020.10.20

---
+++ JavaStudy3/src/Restaurant_BusyWait.java
@@ -0,0 +1,10 @@
+﻿
+public class Restaurant_BusyWait {
+	public static void main(String[] args) {
+		Food_BusyWait food = new Food_BusyWait();
+		Waiter_BusyWait1 Waiter = new Waiter_BusyWait1("웨이터",food);
+		Chef_BusyWait Chef = new Chef_BusyWait("주방장",food);
+		Waiter.start();
+		Chef.start();
+	}
+}
