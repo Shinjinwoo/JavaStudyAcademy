7343596d1bf10bf7243bcbd4d994376807f4c174
Shinjinwoo <jinu712@gmail.com>
2020-10-25 21:04:21 +0900

    2020.10.25

--- JavaStudy3/src/Blue.java
+++
@@ -1,9 +0,0 @@
-
-public class Blue {
-	public void blueFlag() {
-		while (true) {
-			System.out.println("청기 올려 !!");
-		}
-	}
-
-}
--- JavaStudy3/src/White.java
+++
@@ -1,8 +0,0 @@
-
-public class White {
-	public void whiteFlag() {
-		while (true) {
-			System.out.println("백기 올려 !! ");
-		}
-	}
-}
